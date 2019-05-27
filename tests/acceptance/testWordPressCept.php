a5b34133a18e32a14d1a86580df25839218df23e
Steve Henty <stevehenty@gmail.com>
2019-05-27 16:32:23 +0200

    Update testWordPressCept.php

--- tests/acceptance/testWordPressCept.php
+++ tests/acceptance/testWordPressCept.php
@@ -7,7 +7,6 @@ $I->wantTo( 'Test that WordPress works.' );
 $I->amOnUrl( 'https://wordpress.org' );
 
 $I->fillField('search', 'henty');
-$I->pause();
 $I->click('Submit');
 $I->seeCurrentURLEquals('/search/henty');
 $I->click('WordPress › Profiles » Steve Henty');
